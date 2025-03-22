f97dec145c0c8ffbfb731d7dff645b4d72926f03
Jakub Suliga <j.p.suliga@gmail.com>
2025-03-22 15:47:30 +0100

    add: enhance BirdDroneCNN with batch normalization and dropout layers

--- src/model.py
+++ src/model.py
@@ -15,18 +15,23 @@ class MyBirdDroneCNN(nn.Module):
             in_channels=32, out_channels=64, kernel_size=3, padding=1
         )
 
+        self.bn1 = nn.BatchNorm2d(16)
+
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.dropout = nn.Dropout(0.3)
 
         self.fc1 = nn.Linear(64 * 28 * 28, 128)
         self.fc2 = nn.Linear(128, 1)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
 
         x = x.view(x.size(0), -1)  # Flattening
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         x = torch.sigmoid(x)
         return x
