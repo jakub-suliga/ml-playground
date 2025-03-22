ab5ac2eea47af0b491479c292e3d88c9e07e9c0d
Jakub Suliga <j.p.suliga@gmail.com>
2025-03-22 15:28:25 +0100

    add: implement MyBirdDroneCNN model for image classification

---
+++ src/model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MyBirdDroneCNN(nn.Module):
+    # Input shape: (224, 224, 3)
+    def __init__(self):
+        super(MyBirdDroneCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(
+            in_channels=16, out_channels=32, kernel_size=3, padding=1
+        )
+        self.conv3 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=3, padding=1
+        )
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(64 * 28 * 28, 128)
+        self.fc2 = nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+
+        x = x.view(x.size(0), -1)  # Flattening
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = torch.sigmoid(x)
+        return x
